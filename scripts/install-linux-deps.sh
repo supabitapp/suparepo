5374329867ab81ca81cf40f297a69f23ccf33691
khoi <git@khoi.io>
2026-01-19 19:41:16 +0700

    Port SupaIMG GitHub workflows

---
+++ scripts/install-linux-deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y libgtk-3-dev libwebkit2gtk-4.1-dev libayatana-appindicator3-dev librsvg2-dev nasm pkg-config libjpeg-dev libpng-dev
